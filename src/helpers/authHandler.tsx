74c5b09cd9fad4fd9aef54f8298b196af8550d3b
Victor Mann <victorcmann@gmail.com>
2023-03-29 21:20:19 -0300

    private routes and help functions

--- src/helpers/authHandler.tsx
+++ src/helpers/authHandler.tsx
@@ -1,4 +1,6 @@
 import Cookies from 'js-cookie';
+import { Navigate } from 'react-router-dom';
+
 
 export const isLogged = () => {
   let token = Cookies.get('token');
@@ -9,4 +11,14 @@ export const doLogin = (token: string | undefined, rememberPassword: boolean = f
   token = token ?? '';
   if (rememberPassword) Cookies.set('token', token, { expires: 999 });
   else Cookies.set('token', token);
+};
+
+export const doLogout = () => {
+  Cookies.remove('token');
+};
+
+export const routePrivate = (children: JSX.Element): JSX.Element => {
+  return isLogged()
+  ? children
+  : <Navigate to="/signin" />
 };
---
+++ src/helpers/index.ts
@@ -0,0 +1,25 @@
+import { ErrorType } from "../types";
+
+
+/**
+ * Retorna a classe do dado passado: String, Number, Object
+ * 
+ * @param data {any}
+ * @returns {string}
+ */
+export const classOf = (data: any) => {
+	if (data === null) return "Null";
+	if (data === undefined) return "Undefined";
+	return Object.prototype.toString.call(data).slice(8,-1);
+};
+
+/**
+ * Reposta de erro, verifica se o data da response é um objeto, caso não, retorna statusText 
+ * 
+ * @param response {AxiosError}
+ * @returns {object}
+ */
+export const trataErrorResponse = (err: any): ErrorType => {
+	if (classOf(err.response?.data) === 'Object') return err.response.data;
+	return {error: err.response?.statusText};
+};
