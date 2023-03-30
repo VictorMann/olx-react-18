0761c6bc86157b608e8ff26dcc0578d7bd9b353c
Victor Mann <victorcmann@gmail.com>
2023-03-30 12:04:40 -0300

    page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import * as C from './styles';
 
-import { useEffect, useState } from 'react';
-import { CategoryType, UFType } from '../../types';
+import { AdType, CategoryType, UFType } from '../../types';
+import { PageTitle } from '../../components/MainComponents';
+import AdItem from '../../components/AdItem';
 import { api } from '../../Api';
 
 function Page() {
@@ -12,11 +14,13 @@ function Page() {
 
   const [ufs, setUFs] = useState<UFType[]>([]);
   const [categories, setCategories] = useState<CategoryType[]>([]);
+  const [ads, setAds] = useState<AdType[]>([]);
 
   useEffect(() => {
     const fn = async () => {
       const resp = await api.uf();
-      setUFs(resp);
+      if (resp.error) alert(resp.error);
+      else setUFs(resp);
     };
     fn();
   }, []);
@@ -24,7 +28,17 @@ function Page() {
   useEffect(() => {
     const fn = async () => {
       const resp = await api.categoria();
-      setCategories(resp);
+      if (resp.error) alert(resp.error);
+      else setCategories(resp);
+    };
+    fn();
+  }, []);
+
+  useEffect(() => {
+    const fn = async () => {
+      const resp = await api.ads({});
+      if (resp.error) alert(resp.error);
+      else setAds(resp);
     };
     fn();
   }, []);
@@ -42,52 +56,70 @@ function Page() {
 
   return (
     <C.Container>
-      <div className="container">
-        <form onSubmit={handleSubmit} className="p-3 my-4 rounded-3">
+      <div className="area-busca py-4">
+        <div className="container">
+          <form onSubmit={handleSubmit} className="p-3 my-4 rounded-3">
 
-          <div className="row">
+            <div className="row">
 
-            <div className="col-8">
-              <input 
-                className="form-control" 
-                placeholder="O que você procura?"
-                value={q}
-                onChange={handleChangeSearch}
-                required />
+              <div className="col-8">
+                <input 
+                  className="form-control" 
+                  placeholder="O que você procura?"
+                  value={q}
+                  onChange={handleChangeSearch}
+                  required />
+              </div>
+
+              <div className="col-2">
+                <select 
+                  className="form-select"
+                  value={stateLoc}
+                  onChange={handleChangeStateLoc}>
+                    <option>Selecione</option>
+                    {ufs.map(item => (
+                      <option key={item.id} value={item.id}>
+                        {item.id}
+                      </option>
+                    ))}
+                </select>
+              </div>
+
+              <div className="col-2">
+                <button 
+                  className='w-100 btn btn-info text-light fw-bold'>Pesquisar</button>
+              </div>
             </div>
+          </form>
 
-            <div className="col-2">
-              <select 
-                className="form-select"
-                value={stateLoc}
-                onChange={handleChangeStateLoc}>
-                  <option>Selecione</option>
-                  {ufs.map(item => (
-                    <option key={item.id} value={item.id}>
-                      {item.id}
-                    </option>
-                  ))}
-              </select>
-            </div>
+          <ul className="list-unstyled d-flex justify-content-evenly">
+            {categories.map(item => (
+              <li key={item.id}>
+                <Link to={`/ads?category=${item.id}`} className="text-decoration-none">
+                  <img src={item.image} alt="" className='me-3' style={{width: '60px'}} />
+                  <span>{item.description}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
 
-            <div className="col-2">
-              <button 
-                className='w-100 btn btn-info text-light fw-bold'>Pesquisar</button>
-            </div>
+        </div>
+      </div>
+      <div>
+        <div className="container">
+          <PageTitle>Anúncios Recentes</PageTitle>
+          <div className='ad-list mb-3'>
+            {ads.map(item => (
+              <AdItem key={item.id} item={item} />
+            ))}
           </div>
-        </form>
-
-        <ul className="list-unstyled d-flex justify-content-evenly">
-          {categories.map(item => (
-            <li key={item.id}>
-              <Link to={`/ads?category=${item.id}`} className="text-decoration-none">
-                <img src={item.image} alt="" className='me-3' style={{width: '60px'}} />
-                <span>{item.description}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-
+          <div className='text-end pb-2 mb-4 border-bottom'><Link to="/ads" className='btn btn-sm btn-warning'>Ver todos</Link></div>
+          <p className='text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book. It has survived not only five centuries, but also the leap into 
+            electronic typesetting, remaining essentially unchanged.</p>
+        </div>
       </div>
     </C.Container>
   )
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -2,7 +2,17 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 
+.area-busca {
+  background-color: #e1e1e1;
+  border-bottom: 3px solid #DDD;
+}
 form {
   background: #b5cf25;
+}
+
+.ad-list {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 15px;
 }
 `;
