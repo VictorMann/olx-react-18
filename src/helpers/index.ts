942f7a6d6b22e121078c969239bf46a1c943a111
Victor Mann <victorcmann@gmail.com>
2023-03-31 18:34:00 -0300

    created formatLongDate helper function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -27,3 +27,27 @@ export const trataErrorResponse = (err: any): ErrorType => {
 
 	return {error: `${err.message}\n${err.config?.baseURL}${err.config?.url}`};
 };
+
+/**
+ * Formata data por extenso
+ * 
+ * @param d {string}
+ * @returns {string} Exp.: 3 de março 2023
+ */
+export const formatLongDate = (d: string) => {
+	let date: Date = new Date(d);
+	let months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+	return `${date.getDate()} de ${months[date.getMonth()]} de ${date.getFullYear()}`;
+};
+
+/**
+ * Delay de processo
+ * 
+ * @param time {number} tempo em segundos de espera, default 500ms
+ * @returns {null}
+ */
+export const delay = async (time: number = 500) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => resolve(null), time);
+	});
+}
