97e5573b70d1a4dddff5cd5965c2cb2ff0c9575d
Victor Mann <victorcmann@gmail.com>
2023-04-07 12:36:11 -0300

    adjust on home page

--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -14,5 +14,23 @@ form {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 15px;
+
+  figure { min-height: 116.5px; overflow: hidden }
+
+  @media (min-width: 576px) { 
+    figure { min-height: 117.75px; max-height: 117.75px; }
+  }
+  @media (min-width: 768px) { 
+    figure { min-height: 152.75px; max-height: 152.75px; }
+  }
+  @media (min-width: 992px) { 
+    figure { min-height: 212.75px; max-height: 212.75px; }
+  }
+  @media (min-width: 1200px) { 
+    figure { min-height: 275.75px; max-height: 275.75px; }
+  }
+  @media (min-width: 1400px) { 
+    figure { min-height: 302.75px; max-height: 302.75px; }
+  }
 }
 `;
