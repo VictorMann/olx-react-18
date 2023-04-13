941bfb8b4262b27c464863159fe26fb4528bafe3
Victor Mann <victorcmann@gmail.com>
2023-04-12 22:54:37 -0300

    footer data

--- src/components/partials/Footer/index.tsx
+++ src/components/partials/Footer/index.tsx
@@ -3,7 +3,9 @@ import * as C from './styles';
 function Comp() {
   return (
     <C.Container>
-      ...
+      <div className="container">
+        <p>Todos os direitos reservados.</p>
+      </div>
     </C.Container>
   )
 }
--- src/components/partials/Footer/styles.tsx
+++ src/components/partials/Footer/styles.tsx
@@ -1,3 +1,10 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+border-top: 1px solid #DDD;
+margin-top: 1em;
+padding: 1em;
+text-align: center;
+color: #888;
+font-size: .8em;
+`;
