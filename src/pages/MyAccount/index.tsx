79ecdcf6577b9d41c3e10b6eb92f192bff6716b6
Victor Mann <victorcmann@gmail.com>
2023-04-20 17:05:41 -0300

    created MyAccount page

--- src/pages/MyAccount/index.tsx
+++ src/pages/MyAccount/index.tsx
@@ -1,12 +1,110 @@
 import * as C from './styles';
 import { PageTitle } from '../../components/MainComponents';
+import { useEffect, useState } from 'react';
+import { api } from '../../Api';
 
+import { UFType, AdType } from '../../types';
+import AdItem from '../../components/AdItem';
 
 function Page() {
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [uf, setUf] = useState<string>('');
+  
+  const [ads, setAds] = useState<AdType[]>([]);
+  const [ufs, setUFs] = useState<UFType[]>([]);
+
+  const [disabled, setDisabled] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      const resp = await api.myCount();
+      if (resp.error) alert(resp.error);
+      
+      setName(resp.name);
+      setEmail(resp.email);
+      setUf(resp.uf);
+      setAds(resp.ads);
+
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      let resp = await api.uf();
+      if (resp.error) alert(resp.error);
+      else setUFs(resp);
+    })();
+  }, []);
+
+  
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <C.Container>
       <div className="container">
         <PageTitle>Minha Conta</PageTitle>
+
+        <form onSubmit={handleSubmit} className=''>
+
+          <div className="mb-3 d-flex">
+            <label htmlFor="staticName" className="col-form-label">Nome</label>
+            <div className="">
+              <input 
+                id="staticName"
+                className="form-control" 
+                type="text" 
+                onChange={e => setName(e.target.value)}
+                value={name}
+                disabled={disabled}
+                required />
+            </div>
+          </div>
+
+          <div className="mb-3 d-flex">
+            <label htmlFor="staticEmail" className="col-form-label">E-mail</label>
+            <div className="">
+              <input 
+                id="staticEmail"
+                className="form-control" 
+                type="text" 
+                onChange={e => setName(e.target.value)}
+                value={email}
+                disabled={true}
+                required />
+            </div>
+          </div>
+
+          <div className="mb-3 d-flex">
+            <label htmlFor="staticEstado" className="col-form-label">Estado</label>
+            <div className="">
+              <select 
+                id="staticEstado"
+                className="form-select" 
+                onChange={e => setUf(e.target.value)}
+                value={uf}
+                disabled={disabled}
+                required>
+                  <option></option>
+                  {ufs.map(item => (
+                    <option key={item.id} value={item.id}>{item.name}</option>
+                  ))}
+              </select>
+            </div>
+          </div>
+
+        </form>
+
+        <h5>Meus Anúncios</h5>
+        <div className='ad-list mb-3'>
+          {ads.length && ads.map(item => (
+            <AdItem key={item.id} item={item} editable />
+          ))}
+        </div>
+
       </div>
     </C.Container>
   )
--- src/pages/MyAccount/styles.tsx
+++ src/pages/MyAccount/styles.tsx
@@ -1,3 +1,71 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+
+form {
+  padding: 1em;
+
+  & > div {
+    flex-direction: column;
+  }
+  label {
+    margin-right: 1em;
+  }
+}
+.area-remember-pass {
+  flex-direction: row;
+}
+
+.ad-list {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 15px;
+}
+
+@media (min-width: 576px) { 
+  form {
+    & > div {
+      flex-direction: row;
+
+      label {
+        width: 30%;
+        text-align: right;
+
+        & + div {
+          flex: 1;
+        }
+      }
+    }
+  }
+  .ad-list {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+@media (min-width: 768px) { 
+  form {
+    & > div {
+      
+      label {
+        width: 140px;
+
+        & + div {
+          flex: none;
+          width: 350px;
+        }
+      }
+    }
+  }
+  .ad-list {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+@media (min-width: 992px) { 
+
+}
+@media (min-width: 1200px) { 
+
+}
+@media (min-width: 1400px) { 
+
+}
+`;
