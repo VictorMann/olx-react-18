f22f4275b6ef31604811bec73507d561bdcb474f
Victor Mann <victorcmann@gmail.com>
2023-04-20 17:02:59 -0300

    applied action of Modal in AdItem

--- src/components/AdItem/index.tsx
+++ src/components/AdItem/index.tsx
@@ -1,30 +1,60 @@
+import { useDispatch } from 'react-redux';
+import { setAd, setOpen } from '../../redux/reducers/modalAdItemReducer';
 import { Link } from 'react-router-dom';
 import * as C from './styles';
-
-
+import { api } from '../../Api';
 import { AdType } from '../../types';
 
 type Props = {
   item: AdType;
+  editable?: boolean;
 }
 
-function Comp({ item }: Props) {
+function Comp({ item, editable }: Props) {
+  const dispatch = useDispatch();
 
   let price: string = item.priceNegotiable 
   ? 'Preço Negociável'
   : 'R$ ' + item.price;
 
+  const handleClickEdit = async (id: number) => {
+    const resp = await api.ad(id);
+    if (resp.error) alert(resp.error);
+    else {
+      dispatch( setAd(resp) );
+      dispatch( setOpen(true) );
+    }
+  };
+
   return (
     <C.Container className='border rounded p-1'>
-      <Link to={`/ad/${item.id}`} className="text-decoration-none">
-        <figure className='m-0 d-flex align-items-center bg-white' style={{ backgroundImage: `url(${item.image})` }}>
-          <img src={item.image} alt="" className='w-100 rounded' />
-        </figure>
-        <ul className='list-unstyled bg-white m-0 border-top p-2 rounded'>
-          <li className='fw-bold'>{item.title}</li>
-          <li>{price}</li>
-        </ul>
-      </Link>
+      {editable
+        ?
+          <div className="editable">
+            <ul className='editable-menu'>
+              <li><span onClick={() => handleClickEdit(Number(item.id))}>edit</span></li>
+              <li><Link to={`/ad/${item.id}`}>view</Link></li>
+            </ul>
+            <figure className='m-0 d-flex align-items-center bg-white' style={{ backgroundImage: `url(${item.image})` }}>
+              <img src={item.image} alt="" className='w-100 rounded' />
+            </figure>
+            <ul className='list-unstyled bg-white m-0 border-top p-2 rounded'>
+              <li className='fw-bold'>{item.title}</li>
+              <li>{price}</li>
+            </ul>
+          </div>
+        :
+          <Link to={`/ad/${item.id}`} className="text-decoration-none">
+            <figure className='m-0 d-flex align-items-center bg-white' style={{ backgroundImage: `url(${item.image})` }}>
+              <img src={item.image} alt="" className='w-100 rounded' />
+            </figure>
+            <ul className='list-unstyled bg-white m-0 border-top p-2 rounded'>
+              <li className='fw-bold'>{item.title}</li>
+              <li>{price}</li>
+            </ul>
+          </Link>
+      }
+      
     </C.Container>
   )
 }
--- src/components/AdItem/styles.ts
+++ src/components/AdItem/styles.ts
@@ -1,6 +1,55 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+
+.editable {
+  position: relative;
+  border-radius: inherit;
+  overflow: hidden;
+}
+.editable-menu {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  background-color: rgba(0, 0, 0, .3);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  // visibility: hidden;
+  opacity: 0;
+  transition: opacity ease .3s;
+
+  &:hover {
+    // visibility: visible;
+    opacity: 1;
+  }
+
+  & span, & a {
+    display: inline-block;
+    cursor: pointer;
+    color: white;
+    width: 50px;
+    padding: calc(50% - 16px) 0;
+    text-align: center;
+    background: black;
+    margin-left: .5em;
+    border-radius: 50%;
+
+    &:hover {
+      color: var(	--bs-primary);
+    }
+  }
+  
+  a {
+    text-decoration: none;
+  }
+}
+
 figure {
   background-position: center;
   background-repeat: no-repeat;
